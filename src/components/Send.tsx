75f7718da3807d1f0b54a11d43236604d65d32c2
Kamil <kamil.woloszyn1@gmail.com>
2020-12-20 23:00:36 +0100

    Added Conditional Rendering

--- src/components/Send.tsx
+++ src/components/Send.tsx
@@ -2,10 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 interface Props {
-  text: string
+  Active: boolean;
+  Show: boolean;
 }
-export const Send: React.FC<Props> = ({ }) => {
-  return (
+export const Send: React.FC<Props> = (Props) => {
+  if (Props.Active) {
+    return (
     <h1> Hello from Send module</h1>
   )
+  } else {
+    return ( 
+      <div>
+        Module Inactive
+      </div>
+    )
+  }
+  
 }
