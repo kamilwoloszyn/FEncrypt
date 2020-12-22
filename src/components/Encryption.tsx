3f87a1567d360280289d47e32419b737aa56a737
Kamil <kamil.woloszyn1@gmail.com>
2020-12-22 21:43:08 +0100

    Customize encryption form

--- src/components/Encryption.tsx
+++ src/components/Encryption.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import styled from 'styled-components';
+import { Row, Col } from '../styles/layout/layout';
+import { OptionButton } from '../styles/buttons';
 
 const EncryptionWrapper = styled.div<EncryptionWrapperProps>`
 display:${({ Show }) => Show ? 'block':'none'}
@@ -18,7 +20,19 @@ export const Encryption: React.FC<Props> = (Props)=> {
   if (Props.Active) {
     return (
       <EncryptionWrapper Show={Props.Show} > 
-          <h1> Hello from Encryption module</h1>
+        <Row>
+          <Col flex={1}>
+            <h1> Encryption</h1>
+          </Col>
+         </Row>
+        <Row>
+        <form>
+            <input type="password" placeholder="Enter your password here" />
+          <OptionButton type="button" customColor={"#fff"} hoverColor={"#38b13b"} borderColor={"#38b13b"}>Encrypt !</OptionButton>
+        </form>
+        </Row>
+        
+     
         </EncryptionWrapper>
       )
   } else { 
