08a4f3a5f872619de4ca65656005b1fb5c971b9a
Kamil <kamil.woloszyn1@gmail.com>
2020-12-23 12:46:46 +0100

    Small cosmetic changes

--- src/components/Encryption.tsx
+++ src/components/Encryption.tsx
@@ -1,8 +1,10 @@
+import '../styles/scss/forms.scss';
 import React from "react";
 import ReactDOM from "react-dom";
 import styled from 'styled-components';
 import { Row, Col } from '../styles/layout/layout';
 import { OptionButton } from '../styles/buttons';
+import { FormGroup, FormLabelBlock, FormInputBlock } from '../styles/forms'
 
 const EncryptionWrapper = styled.div<EncryptionWrapperProps>`
 display:${({ Show }) => Show ? 'block':'none'}
@@ -26,10 +28,17 @@ export const Encryption: React.FC<Props> = (Props)=> {
           </Col>
          </Row>
         <Row>
-        <form>
-            <input type="password" placeholder="Enter your password here" />
-          <OptionButton type="button" customColor={"#fff"} hoverColor={"#38b13b"} borderColor={"#38b13b"}>Encrypt !</OptionButton>
-        </form>
+          <FormGroup>
+            <form>
+              <FormLabelBlock>
+                <label htmlFor="encryptpassword">Enter password you want to decrypt file later.</label>
+              </FormLabelBlock>
+              <FormInputBlock>
+                <input type="text" name="encryptpassword" className="form-input" placeholder="Enter your password here" />
+              </FormInputBlock>
+                <OptionButton type="button" customColor={"#fff"} hoverColor={"#38b13b"} borderColor={"#38b13b"}>Encrypt !</OptionButton>
+            </form>
+            </FormGroup>
         </Row>
         
      
