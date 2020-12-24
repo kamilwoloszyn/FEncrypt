13089ab41a6c0e0209b8c6bf978c3dce831707dc
Kamil <kamil.woloszyn1@gmail.com>
2020-12-24 15:39:43 +0100

    Added initial progress bar

--- src/components/shared/Progress.tsx
+++ src/components/shared/Progress.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import * as ReactDOM from "react-dom";
 import styled from 'styled-components';
-
+import '../../styles/scss/progress.scss'
 interface Props { 
   Active: boolean;
   Show: boolean;
@@ -20,7 +20,11 @@ export const Progress: React.FC<Props> = (Props) => {
   if (Props.Active) { 
     return (
       <ProgressWrapper Show={Props.Show}>
-        <h1> Hello from Progress module</h1>
+        <div className="steps-group">
+          <span className="step active">1</span>
+          <span className="step">2</span>
+          <span className="step">3</span>
+        </div>
       </ProgressWrapper>
     
   )
