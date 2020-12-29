eee8b8b11924fbf3ae493d070348399b544cc051
Kamil <kamil.woloszyn1@gmail.com>
2020-12-29 12:58:05 +0100

    Added action context

--- src/context/PasswordContext.tsx
+++ src/context/PasswordContext.tsx
@@ -3,11 +3,22 @@ import { createContext, useState} from 'react';
 export interface PasswordState {
   usedPassword: string,
   SetUsedPassword: React.Dispatch<React.SetStateAction<string>>
-} 
+}
+
+export interface ActionToDo {
+  encrypt: boolean;
+  decrypt: boolean;
+}
 
 const initialState: PasswordState=  {
   usedPassword: "",
   SetUsedPassword: ()=> console.warn("No SetUsedPassword provided.")
 }
 
+const initialStateActionToDo: ActionToDo = { 
+  encrypt: false,
+  decrypt: false
+}
+
 export const PasswordContext = createContext<PasswordState>(initialState)
+export const ActionToDoContext = createContext<ActionToDo>(initialStateActionToDo)
