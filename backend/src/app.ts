2bc19db2ecfdf812f7de649bcae6d1743009f4f1
Kamil <kamil.woloszyn1@gmail.com>
2020-12-31 21:37:51 +0100

    App now use BodyParser module

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import { PORT, HOST } from './config/server';
 import DecryptionRoute  from './routes/decrypt';
 import EncryptionRoute  from './routes/encrypt';
- 
+
 const app = express();
+app.use(bodyParser);
 
 app.get('/', (req, res) => {
   res.writeHead(200);
