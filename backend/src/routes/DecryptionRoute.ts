3934aeda82e4c5717408a7c87463135441acb1ce
Kamil <kamil.woloszyn1@gmail.com>
2021-01-01 23:41:50 +0100

    Connected routes.

--- backend/src/routes/DecryptionRoute.ts
+++ backend/src/routes/DecryptionRoute.ts
@@ -1,7 +1,8 @@
 import * as socketio from 'socket.io';
+import { Decrypt } from '../controller/CryptoController';
 
-function DecryptionRoute(key: string, buf: Buffer): void   {
-  
+function DecryptionRoute(key: string, buf: string, iv: string): void   {
+  const decrypted: string = Decrypt(key, buf, iv);
 }
 
 export default DecryptionRoute;
--- backend/src/routes/EcryptionRoute.ts
+++ backend/src/routes/EcryptionRoute.ts
@@ -1,6 +1,7 @@
 import * as socketio from 'socket.io';
-function EncryptionRoute(key: string, buf: Buffer): void {
-  
-}
+import { Encrypt } from '../controller/CryptoController';
 
+function EncryptionRoute(key: string, buf: string): void {
+  let encrypted: {iv:string, buf:string} = Encrypt(key,buf) 
+}
 export default EncryptionRoute;
