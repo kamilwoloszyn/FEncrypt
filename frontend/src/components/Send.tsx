af8b0847c9a7a3db8a97c14dea54cb44cee951ed
Kamil <kamil.woloszyn1@gmail.com>
2021-01-01 16:56:53 +0100

    Added uploading data via socket.io

--- frontend/src/components/Send.tsx
+++ frontend/src/components/Send.tsx
@@ -1,10 +1,12 @@
 import React,{useEffect,useContext} from "react";
 import styled from 'styled-components'
+import socketIOClient  from "socket.io-client";
 import {useDispatch} from 'react-redux'
 import { SetStep } from '../redux/action';
 import { PasswordContext, PasswordState, ActionToDoContext, ActionToDo, FileUploadContext } from '../context/context';
 import { Col } from '../styles/layout/layout';
 import '../styles/scss/shared/modal.scss';
+import { Socket } from "socket.io-client";
 
 interface Props {
   Active: boolean;
@@ -14,7 +16,8 @@ interface Props {
 interface SendWrapperProps { 
   Show: boolean;
 }
-
+const EncryptPoint = "http://localhost:3000/encrypt"
+const DecryptPoint = "http://localhost:3000/decrypt"
 const SendWrapper = styled.div<SendWrapperProps>`
 display:${({Show})=> Show? 'block':'none'}
 `
@@ -29,7 +32,31 @@ export const Send: React.FC<Props> = (Props) => {
       dispatch(SetStep(2))
     })
   
-    useEffect(() => {
+  useEffect(() => {
+    if (globalPassword && globalAction && globalFile) {
+      switch (globalAction.usedContext) {
+        case 'encrypt': {
+          const socket = socketIOClient(EncryptPoint);
+          socket.on("connection", (sock: SocketIOClient.Socket) =>{
+            socket.emit('data-client', {
+              password: globalPassword,
+              file: globalFile,
+            })
+          })
+         
+        }
+          case 'decrypt': { 
+          const socket = socketIOClient(DecryptPoint);
+          socket.emit('data-client', {
+            password: globalPassword,
+            file : globalFile
+          })
+        }
+        default: {
+          console.log("Operation not recognised")
+          } 
+        }
+      }
         console.log(globalFile?.usedFile)
       })
   
