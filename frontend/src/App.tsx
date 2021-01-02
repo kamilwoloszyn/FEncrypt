4310c10c83d10c54e8b7c7da897329f22229478b
Kamil <kamil.woloszyn1@gmail.com>
2021-01-02 19:17:50 +0100

    Removed unused depency

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,7 @@ import { Decryption } from './components/Decryption';
 import { Send } from './components/Send';
 import { Col, Row,Container } from './styles/layout/layout';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { PasswordContext, ActionToDoContext, FileUploadContext, IVContext, IvState } from './context/context';
+import { PasswordContext, ActionToDoContext, FileUploadContext, IVContext } from './context/context';
 
 export const App: React.FC = (ContainerOption) => {
   const [usedPassword, SetUsedPassword] = useState<string>("");
