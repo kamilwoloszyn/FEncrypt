49872a0f27c6a8d43c1432f5ab6d917eb5925d39
Kamil <kamil.woloszyn1@gmail.com>
2021-01-02 10:07:26 +0100

    Added handle events via socket.io

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,29 +8,57 @@ const http = require("http").Server(app);
 const io = require("socket.io")(http)
 
 io.on("connection", function (socket : socketio.Socket) { 
-  socket.on("data-client", (data: {file :Buffer, password: string, context:string,iv?:string}) => {
-    if (data.file.length > 0 && data.password.length > 5 && data.context.length > 0)  { 
-      switch (data.context) { 
+  socket.on("data-client", (data: { file: Buffer, password: string, context: string, iv?: string }) => {
+    let socketId = socket.id
+    if (data.file.length > 0 && data.password.length > 5 && data.context.length > 0) {
+      switch (data.context) {
         case "encrypt": {
-           EncryptionRoute(data.password,data.file)
+          EncryptionRoute(data.password, data.file).then((data) => {
+            let socketId = socket.id;
+           })
         }
-        break;
+          break;
         case "decrypt": {
           if (data.iv) {
-            DecryptionRoute(data.password,data.file,data.iv)
-          } else { 
-            //return iv is missing
+            DecryptionRoute(data.password, data.file, data.iv).then((buf) => {
+              io.to(socketId).emit('server-response', {
+                statusCode: 200,
+                message: {
+                  str: "All ok"
+                },
+                buffer: buf
+              })
+            })
+          } else {
+            io.to(socketId).emit('server-response', {
+              statusCode: 400,
+              message: {
+                str: "IV is empty"
+              }
+            })
           }
         }
-        break;
+          break;
         default: {
-          // send 400 (Bad request)
-        }
-        }
-    } else { 
-      //send respond to client that request is not complete
+          io.to(socketId).emit('server-response', {
+            statusCode: 400,
+            message: {
+              str:"Bad action context"
+            }
+          })
+          }
       }
+
+    } else {
+      io.to(socketId).emit('server-response', {
+        statusCode: 400,
+        message: {
+          str: "400 Bad request"
+        }
+      })
+    }
   })
+
 })
 
 http.listen(process.env.PORT || PORT, function () {
