8077ae82086f06a4e159e2246286c558823ad762
Kamil <kamil.woloszyn1@gmail.com>
2021-01-03 18:46:25 +0100

    Hotfix: Encryption and Decryption error due to wrong key length

--- backend/src/controller/CryptoController.ts
+++ backend/src/controller/CryptoController.ts
@@ -5,7 +5,8 @@ const alghoritm = `aes-256-cbc`;
 export const Decrypt = (key: string, encryptedData: Buffer, iv: string): Promise<Buffer> => {
   return new Promise<Buffer>((resolve,reject)=> { 
     let ivBuf: Buffer = Buffer.from(iv);
-    let decipher: crypto.Decipher = crypto.createDecipheriv(alghoritm, Buffer.from(key), ivBuf);
+    let hashKey = crypto.createHash('sha256').update(String(key)).digest('base64').substr(0, 32);
+    let decipher: crypto.Decipher = crypto.createDecipheriv(alghoritm, Buffer.from(hashKey), ivBuf);
     let decrypted : Buffer = decipher.update(encryptedData);
     decrypted = Buffer.concat([decrypted, decipher.final()]);
     if (!decrypted) {
@@ -19,13 +20,13 @@ export const Decrypt = (key: string, encryptedData: Buffer, iv: string): Promise
 export const Encrypt = (key: string, buf: Buffer): Promise<{ iv: string, buf: Buffer }> => { 
   return new Promise<{ iv: string, buf: Buffer }>((resolve, reject) => {
       let iv: Buffer = crypto.randomBytes(16);
-      let cipher: crypto.Cipher = crypto.createCipheriv(alghoritm, Buffer.from(key, 'utf-8'), iv)
+      let hashKey = crypto.createHash('sha256').update(String(key)).digest('base64').substr(0, 32);
+      let cipher: crypto.Cipher = crypto.createCipheriv(alghoritm, Buffer.from(hashKey), iv)
       let encrypted : Buffer = cipher.update(buf);
     encrypted = Buffer.concat([encrypted, cipher.final()]);
     if (!encrypted) {
       reject(new Error("An error occured during decryption"))
     }
     resolve({iv: iv.toString('hex'),buf: encrypted})
-    
   })
 }
