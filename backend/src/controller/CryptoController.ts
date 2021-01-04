df243b8af3896d2efda5153ba6160040ba8166d6
Kamil <kamil.woloszyn1@gmail.com>
2021-01-04 21:23:48 +0100

    Fixed enrypt & decrypt errros

--- backend/src/controller/CryptoController.ts
+++ backend/src/controller/CryptoController.ts
@@ -2,28 +2,23 @@ import crypto from 'crypto';
 
 const alghoritm = `aes-256-cbc`;
 
-export const Decrypt = (key: string, encryptedData: Buffer, iv: string): Promise<Buffer> => {
-  return new Promise<Buffer>((resolve,reject)=> { 
-    let ivBuf: Buffer = Buffer.from(iv);
-    let hashKey = crypto.createHash('sha256').update(String(key)).digest('base64').substr(0, 32);
-    let decipher: crypto.Decipher = crypto.createDecipheriv(alghoritm, Buffer.from(hashKey), ivBuf);
-    let decrypted : Buffer = decipher.update(encryptedData);
+export const Decrypt = (key: string, encryptedData: Buffer, ivStr: string): Promise<Buffer> => {
+  return new Promise<Buffer>((resolve, reject) => {
+    const salt = crypto.scryptSync(key, 'salt', 32);
+    const iv = Buffer.from(ivStr, 'hex');
+    const decipher = crypto.createCipheriv(alghoritm, salt, iv);
+    let decrypted: Buffer = decipher.update(encryptedData);
     decrypted = Buffer.concat([decrypted, decipher.final()]);
-    if (!decrypted) {
-      reject(new Error("An error occured during decryption"));
-    }
     resolve(decrypted);
   })
-
 }
-
 export const Encrypt = (key: string, buf: Buffer): Promise<{ iv: string, buf: Buffer }> => { 
   return new Promise<{ iv: string, buf: Buffer }>((resolve, reject) => {
-      let iv: Buffer = crypto.randomBytes(16);
-      let hashKey = crypto.createHash('sha256').update(String(key)).digest('base64').substr(0, 32);
-      let cipher: crypto.Cipher = crypto.createCipheriv(alghoritm, Buffer.from(hashKey), iv)
-      let encrypted : Buffer = cipher.update(buf);
-    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    const iv: Buffer = crypto.randomBytes(16);
+    const salt: Buffer = crypto.scryptSync(key,'salt',32)
+    const cipher: crypto.Cipher = crypto.createCipheriv(alghoritm, salt, iv);
+    let encrypted: Buffer = cipher.update(buf);
+    encrypted = Buffer.concat([encrypted,cipher.final()])
     if (!encrypted) {
       reject(new Error("An error occured during decryption"))
     }
